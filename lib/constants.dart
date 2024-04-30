5ad50ff1c4ab4c501dd8ffa6e5f3ea297f9d81ef
ftynfrhn <elfarhana900@gmail.com>
2024-04-30 09:16:13 +0800

    create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// define a set of constants that will be used throughout the app
+const kPrimaryColor = Color(0xFF0D47A1);
+const kPrimaryLightColor = Color(0xFFBBDEFB);
+
+const double defaultPadding = 16.0; // commonly used to add space around UI elements
