59a7e1d41ca9003e65a96fabfc22c9e7978ac76c
ftynfrhn <elfarhana900@gmail.com>
2024-05-03 23:37:26 +0800

    user dashboard

--- lib/Screens/Dashboard/user_dashboard.dart
+++ lib/Screens/Dashboard/user_dashboard.dart
@@ -55,40 +55,15 @@ class UserDashboard extends StatelessWidget {
   }
 }
 
-class ClassScheduleTable extends StatelessWidget {
-  final List<List<String>> timetable = [
-    ['8:30 AM - 9:50 AM', '', '', '', '', ''],
-    ['10:00 AM', '', '', '', '', ''],
-    ['11:30 AM', '', '', '', '', ''],
-    [' ', 'Lunch', 'Lunch', 'Lunch', 'Lunch', 'Lunch'],
-    ['2:00 PM', '', '', '', '', ''],
-    ['3:30 PM', '', '', '', '', ''],
-  ];
+class TimeSlotTile extends StatelessWidget {
+  final String time;
 
   const TimeSlotTile({super.key, required this.time});
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: [
-        DataColumn(label: Text('Time')),
-        DataColumn(label: Text('Monday')),
-        DataColumn(label: Text('Tuesday')),
-        DataColumn(label: Text('Wednesday')),
-        DataColumn(label: Text('Thursday')),
-        DataColumn(label: Text('Friday')),
-      ],
-      rows: List<DataRow>.generate(
-        timetable.length,
-        (index) => DataRow(
-          cells: List<DataCell>.generate(
-            timetable[index].length,
-            (cellIndex) => DataCell(
-              Text(timetable[index][cellIndex]),
-            ),
-          ),
-        ),
-      ),
+    return ListTile(
+      title: Text(time),
     );
   }
 }
