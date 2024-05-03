8fbf4b0b2ce75e744aeba8a8e553ed6b0dbe8c97
ftynfrhn <elfarhana900@gmail.com>
2024-05-03 23:28:01 +0800

    test

--- lib/Screens/Dashboard/user_dashboard.dart
+++ lib/Screens/Dashboard/user_dashboard.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
+import 'course_schedule_page.dart'; 
 
 class UserDashboard extends StatelessWidget {
+  const UserDashboard({super.key, Key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Dashboard'),
+        title: const Text('User Dashboard'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Class Schedule',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              ClassScheduleTable(),
-              // Add other widgets or sections of the user dashboard here
-            ],
+      body: Column(
+        children: [
+          const Text(
+            'Class Timetable',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5, // Number of days (Monday to Friday)
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Day ${index + 1}'),
+                  subtitle: const Column(
+                    children: [
+                      TimeSlotTile(time: '8:30 AM - 9:50 AM'),
+                      TimeSlotTile(time: '10:00 AM - 11:20 AM'),
+                      TimeSlotTile(time: '11:30 AM - 12:50 PM'),
+                      TimeSlotTile(time: '2:00 PM - 3:20 PM'),
+                      TimeSlotTile(time: '3:30 PM - 4:50 PM'),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the CourseSchedulePage when the FAB is pressed
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CourseSchedulePage()),
+          );
+        },
+        child: const Icon(Icons.calendar_today),
       ),
     );
   }
@@ -41,27 +65,7 @@ class ClassScheduleTable extends StatelessWidget {
     ['3:30 PM', '', '', '', '', ''],
   ];
 
-  void insertCourse(int dayIndex, String time, String courseCode) {
-    int timeIndex = 0;
-    switch (time) {
-      case '8:30 AM - 9:50 AM':
-        timeIndex = 1;
-        break;
-      case '10:00 AM':
-        timeIndex = 2;
-        break;
-      case '11:30 AM':
-        timeIndex = 3;
-        break;
-      case '2:00 PM':
-        timeIndex = 4;
-        break;
-      case '3:30 PM':
-        timeIndex = 5;
-        break;
-    }
-    timetable[dayIndex][timeIndex] = courseCode;
-  }
+  const TimeSlotTile({super.key, required this.time});
 
   @override
   Widget build(BuildContext context) {
--- lib/Screens/Login/admin_login.dart
+++ lib/Screens/Login/admin_login.dart
@@ -1,23 +1,135 @@
 import 'package:flutter/material.dart';
-
+import '../../Responsive.dart';
 import '../../Component/background.dart';
 import '../../../constants.dart';
-import '../../Responsive.dart';
 import 'login_form.dart';
+import 'user_login.dart'; // Import the AdminDashboard page
 
-// ana roughly create ni sebab nak avoid error, edit la 
 class AdminLogin extends StatelessWidget {
-  const AdminLogin({Key? key}) : super(key: key);
+  const AdminLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Background(
+    bool isAdmin = true; // Set isAdmin to true for admin login
+    return Background(
       child: SingleChildScrollView(
-        // child: Responsive(
-        //   mobile: MobileLoginScreen(isAdmin: true),
-        //   desktop: DesktopLoginScreen(isAdmin: true),
-        // ),
+        child: SafeArea(
+          child: Responsive(
+            mobile: MobileLoginScreen(isAdmin: isAdmin),
+            desktop: DesktopLoginScreen(isAdmin: isAdmin),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class MobileLoginScreen extends StatelessWidget {
+  final bool isAdmin;
+  const MobileLoginScreen({
+    Key? key,
+    required this.isAdmin,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const LoginScreenTopText(),
+        Row(
+          children: [
+            const Spacer(),
+            Expanded(
+              flex: 8,
+              child: LoginForm(isAdmin: isAdmin),
+            ),
+            const Spacer(),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: GestureDetector(
+            onTap: () {
+              // Navigate to the UserLogin page when "Login As User" button is tapped
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const UserLogin()),
+              );
+            },
+            child: const Text(
+              "Login As User",
+              style: TextStyle(color: kPrimaryColor),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class DesktopLoginScreen extends StatelessWidget {
+  final bool isAdmin;
+  const DesktopLoginScreen({
+    Key? key,
+    required this.isAdmin,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const Expanded(
+          child: LoginScreenTopText(),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 450,
+                child: LoginForm(isAdmin: isAdmin),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: GestureDetector(
+                  onTap: () {
+                    // Navigate to the UserLogin page when "Login As User" button is tapped
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const UserLogin()),
+                    );
+                  },
+                  child: const Text(
+                    "Login As User",
+                    style: TextStyle(color: kPrimaryColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LoginScreenTopText extends StatelessWidget {
+  const LoginScreenTopText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          "ADMIN LOGIN",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+        ),
+        SizedBox(height: defaultPadding * 4),
+      ],
     );
   }
 }
