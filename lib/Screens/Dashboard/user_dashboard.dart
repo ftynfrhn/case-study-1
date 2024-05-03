0f09234f092cbc28bb2c0f07667b73030e3fc709
balqishaifa <133851030+balqishaifa@users.noreply.github.com>
2024-05-03 17:30:25 +0800

    Update user_dashboard.dart

--- lib/Screens/Dashboard/user_dashboard.dart
+++ lib/Screens/Dashboard/user_dashboard.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
+import 'course_schedule_page.dart'; 
 
 class UserDashboard extends StatelessWidget {
+  const UserDashboard({super.key, Key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Dashboard'),
+        title: const Text('User Dashboard'),
       ),
       body: Column(
         children: [
-          Text(
+          const Text(
             'Class Timetable',
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
               itemCount: 5, // Number of days (Monday to Friday)
               itemBuilder: (context, index) {
                 return ListTile(
                   title: Text('Day ${index + 1}'),
-                  subtitle: Column(
+                  subtitle: const Column(
                     children: [
                       TimeSlotTile(time: '8:30 AM - 9:50 AM'),
                       TimeSlotTile(time: '10:00 AM - 11:20 AM'),
@@ -38,6 +41,16 @@ class UserDashboard extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to the CourseSchedulePage when the FAB is pressed
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CourseSchedulePage()),
+          );
+        },
+        child: const Icon(Icons.calendar_today),
+      ),
     );
   }
 }
@@ -45,7 +58,7 @@ class UserDashboard extends StatelessWidget {
 class TimeSlotTile extends StatelessWidget {
   final String time;
 
-  const TimeSlotTile({required this.time});
+  const TimeSlotTile({super.key, required this.time});
 
   @override
   Widget build(BuildContext context) {
